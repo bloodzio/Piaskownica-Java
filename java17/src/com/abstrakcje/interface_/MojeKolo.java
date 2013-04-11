bb2d1a93818e2a9b6864439dbc9991e52c971a92
Leszek Uchacz <bloodzio@gmail.com>
2013-04-11 22:51:05 +0200

    ok

---
+++ java17/src/com/abstrakcje/interface_/MojeKolo.java
@@ -0,0 +1,29 @@
+package com.abstrakcje.interface_;
+
+public class MojeKolo extends Kolo
+{
+
+	public MojeKolo(double r)
+	{
+		super(r);
+	}
+
+	public MojeKolo()
+	{
+		this((double) 1);
+	}
+
+	public double obliczObwod()
+	{
+		double r = getPromien();
+		return r * 10000;
+	}
+
+	public static void main(String[] args)
+	{
+		MojeKolo mk1 = new MojeKolo(10);
+		System.out.println(mk1.obliczObwod());
+		mk1.setKolor("c");
+	}
+
+}
