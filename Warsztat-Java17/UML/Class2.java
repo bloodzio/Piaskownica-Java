2029ffd266366c9c6d3d9dc2c9e27fe0d9835582
Leszek Uchacz <bloodzio@gmail.com>
2013-05-04 00:07:25 +0200

    uml

---
+++ Warsztat-Java17/UML/Class2.java
@@ -0,0 +1,16 @@
+public class Class2 implements Class3 {
+    private int attr1;
+    private int attr2;
+    private int attr3;
+    private int attr4;
+
+    public void operation1(){
+    }
+
+    public void operation2(){
+    }
+
+    public void operation3(){
+    }
+
+}
---
+++ Warsztat-Java17/UML/Class3.java
@@ -0,0 +1,15 @@
+public class Class3 {
+    private int attr1;
+    private int attr2;
+    private int attr3;
+
+    public void operation1(){
+    }
+
+    public void operation2(){
+    }
+
+    public void operation3(){
+    }
+
+}
